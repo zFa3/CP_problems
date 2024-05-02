c96b9909396a149a96f99865e7007b7dae8ed0ec
zFa3 <153332084+zFa3@users.noreply.github.com>
2024-05-02 12:57:20 -0400

    Added Daily Codeforces

---
+++ 1968A_CF_Maximize.cpp
@@ -0,0 +1,16 @@
+/*
+ID: zFa3
+TASK: Maximize?
+LANG: C++
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a, x;
+    cin >> a;
+    for (int i = 0; i < a;  i++){
+        cin >> x;
+        cout << x - 1 << endl;
+    }
+    return 0;
+}
