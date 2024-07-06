5ad8f24e3a188a2cd5e32ebeb99938b244a062ab
zFa3 <153332084+zFa3@users.noreply.github.com>
2024-07-06 19:21:45 -0400

    practice

---
+++ 579A_CF_Bacteria.cpp
@@ -0,0 +1,16 @@
+/*
+ID: zFa3
+TASK: Raising Bacteria
+LANG: C++
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    ll t;
+    cin>>t;
+    cout<<__popcount(t)<<'\n';
+}
