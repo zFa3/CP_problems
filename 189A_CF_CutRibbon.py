f19995cdbfdfa7cf7ff779a5e2dfb7996d42cb87
zFa3 <153332084+zFa3@users.noreply.github.com>
2024-09-01 10:26:43 -0400

    Add files via upload

---
+++ 189A_CF_CutRibbon.py
@@ -0,0 +1,7 @@
+a, b, c, d = map(int, input().split())
+maximum = 1
+for i in range(a//b + 1):
+    for j in range(a//c + 1):
+        if (a - (i * b + j * c)) % d == 0 and i * b + j * c <= a:
+            maximum = max(maximum, i + j + (a - i * b - j * c) // d)
+print(maximum)
