01119b8d1bb2f9422a14c5aebff380171bd77249
zFa3 <153332084+zFa3@users.noreply.github.com>
2024-09-14 13:38:04 -0400

    Add files via upload

--- 189A_CF_CutRibbon.py
+++ 189A_CF_CutRibbon.py
@@ -1,7 +1,14 @@
+import sys
+sys.setrecursionlimit(10000)
+
 a, b, c, d = map(int, input().split())
-maximum = 1
-for i in range(a//b + 1):
-    for j in range(a//c + 1):
-        if (a - (i * b + j * c)) % d == 0 and i * b + j * c <= a:
-            maximum = max(maximum, i + j + (a - i * b - j * c) // d)
-print(maximum)
+
+memo = {}
+def maximum(x):
+    if x == 0:
+        return 0
+    if x in memo:
+        return memo[x]
+    return max(maximum(x - b) + 1, maximum(x - c) + 1, maximum(x - d) + 1)
+
+print(maximum(a))
