6cfcfb7802a904a79cc833977533808757f29661
zFa3 <153332084+zFa3@users.noreply.github.com>
2024-09-20 22:16:35 -0400

    made some code actually work

--- 1915C_CF_CanISquare.cpp
+++ 1915C_CF_CanISquare.cpp
@@ -1,35 +1,47 @@
 /*
 ID: zFa3
-TASK: Can I Square?
+DATE: 2024-09-19 08:27:17
+TASK: Save More Mice
 LANG: C++
 */
 
-#include<bits/stdc++.h>
-#define ll long long
+#include <bits/stdc++.h>
 using namespace std;
 
-void solution(){
-    ll a, b, c=0;
-    cin>>a;
-    for (size_t i = 0; i < a; i++)
-    {
-        cin>>b;
-        c+=b;
+#define SORT(a) sort(a.begin(), a.end())
+#define FOR(a) for(int i = 0; i < a; i++)
+#define For(a, b, c) for(int i = a; i < b; i += c)
+#define SUM(a) accumulate(a.begin(), a.end(), 0LL)
+#define MAX(a) max_element(a.begin(), a.end())
+#define pb push_back
+
+#define all(x) x.begin(), x.end()
+#define f firsrt
+#define s second
+
+#define zFa3 ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+bool check(string inp, int length){
+    if (inp == "1" or inp == "1111") return true;
+    int index = 0;
+    while (index < length){
+        if (inp[index] == '0'){
+            return (index - 1) == (int)pow(inp.length(), 0.5) and (int)pow(inp.length(), 0.5) == pow(inp.length(), 0.5);
+        }
+        index++;
     }
-    if (pow(c, 0.5)==(int)pow(c, 0.5)){
-        cout<<"YES\n";
-    } else {
-        cout<<"NO\n";
-    }
-    
+    return false;
 }
 
 int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
-    ll t;
-    cin>>t;
-
-    while(t--){
-        solution();
+    zFa3
+    int a;
+    cin >> a;
+    while (a--){
+        int length;
+        cin >> length;
+        string input;
+        cin >> input;
+        cout << (check(input, length) ? "YES\n" : "NO\n");
     }
 }
