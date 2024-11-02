35f30ea44b18b4f696fe3c83c1a6d0c16ac04802
Jayden <153332084+zFa3@users.noreply.github.com>
2024-11-02 13:57:22 -0400

    Add files via upload

--- a1.py
+++ a1.py
@@ -1,3 +1,12 @@
-for i in range(int(input())):
-    if input().count('it'): print("YES")
-    else:  print("NO")
+def ad():
+    a = list(map(int, input().split()))
+    prev = a[0] - 5
+    for i in a:
+        if not (abs(i - prev) == 5 or abs(i - prev) == 7):
+            return False
+        prev = i
+    return True
+
+for _ in range(int(input())):
+    input()
+    print("YES" if ad() else "NO")
--- a2.py
+++ a2.py
@@ -1,2 +1,12 @@
-a, b, c = map(int, input().split())
-print(min(a - b, c + 1))
+for _ in range(int(input())):
+    shelves, bottles = map(int, input().split())
+    brands = {}
+    for i in range(bottles):
+        brand, value = map(int, input().split())
+        if brand in brands:
+            brands[brand] += value
+        else:
+            brands[brand] = value
+    b = [brands[item] for item in brands.keys()]
+    b = sorted(b, reverse = True)
+    print(sum(b[:min(len(b), shelves)]))
