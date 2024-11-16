6de68de614ccf3bba3e593d3a09f1b656d2466be
Jayden <153332084+zFa3@users.noreply.github.com>
2024-11-16 08:58:41 -0500

    Added comments, simplified code

--- CCC12S5.py
+++ CCC12S5.py
@@ -1,18 +1,25 @@
+# mouse journey
+
 def paths(row, col):
-    # print(row, col)
+    # if we have already calculated this path
     if (row, col) in memo:
+        # return the previously calculated path
         return memo[(row, col)]
+    
+    # if we are at the bottom right, return 1 path
     if row == rows and col == cols:
-        memo[(row, col)] = 1
         return 1
+    
+    # if we are in a cell with a cat, return 0 paths
     if (row, col) in cats:
-        memo[(row, col)] = 0
         return 0
+    
+    # otherwise, we can recursively check how many paths there are
     total = 0
     if row != rows:
-        total += paths(row + (not row == rows), col)
+        total += paths(row + 1, col)
     if col != cols:
-        total += paths(row, col + (not col == cols))
+        total += paths(row, col + 1)
     memo[(row, col)] = total
     return total
 
