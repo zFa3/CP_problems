540bbb4d13f44c9bc8e2662a852a63c5932841a1
Jayden <153332084+zFa3@users.noreply.github.com>
2025-02-18 22:12:58 -0500

    Update CCC '05 J5 - Bananas.py

--- CCC-Prep/CCC '05 J5 - Bananas.py
+++ CCC-Prep/CCC '05 J5 - Bananas.py
@@ -1,38 +1,21 @@
-import sys
-sys.setrecursionlimit(10000)
-# split based off 'N'?
-# if the first character is a 'B' then we search through the string
-# tally the number of 'B's and 'S's
-#
 def solve(i: str):
-    if len(i) == 0:
-        return True
-    if len(i) == 1:
-        if i == 'A':
-            return True
-        else:
-            return False
-    if len(i) == 2:
-        return False
+    if len(i) == 0: return True
+    if len(i) == 1: return i == 'A'
+    if len(i) == 2: return False
     if i[0] == 'B':
         bs = 0
         for n, j in enumerate(i[1:]):
-            if j == 'B':
-                bs += 1
+            if j == 'B': bs += 1
             elif j == 'S':
                 bs -= 1
                 if (bs == -1):
-                    a = solve(i[1:n + 1])
-                    b = ((solve(i[n + 3:]) and i[n + 2] == 'N') if n + 2 != len(i) else True)
-                    return a and b
+                    return solve(i[1:n + 1]) and ((solve(i[n + 3:]) and i[n + 2] == 'N') if n + 2 != len(i) else True)
         return False
-    try:
-        splitter = i.index("N")
+    try: splitter = i.index("N")
     except: return False
     return solve(i[0:splitter]) and solve(i[splitter + 1:])
 
 while True:
     i = input()
-    if i == 'X':
-        break
+    if i == 'X': break
     print("YES" if solve(i) else "NO")
